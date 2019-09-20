7e221c4922cbbf669990610e9b7705835e7b2949
Dick Loveland <richard.k.loveland@gmail.com>
2019-09-20 10:44:37 -0600

    fixed the game so it looks neater (still terrible code and must be refactored)

--- SpaceFruit/spaceFruit.py
+++ SpaceFruit/spaceFruit.py
@@ -58,6 +58,18 @@ class Penguin(pygame.sprite.Sprite):
 		self.state = 0
 		self.rect = self.image.get_rect()
 		self.rect.move(self.xPos, self.yPos)
+	
+	def update(self):
+		if self.state == 0:
+			self.yPos = 50
+			
+		elif self.state == 1:
+			self.yPos = 150
+			
+		elif self.state ==2:
+			self.yPos = 280
+			
+		self.rect.move(self.xPos, self.yPos)
 		
 class spaceFruit(object):
 	def __init__(self):
@@ -76,31 +88,40 @@ class spaceFruit(object):
 		self.asteroidGroup = pygame.sprite.Group(self.asteroid1, self.asteroid2)
 		self.appleGroup = pygame.sprite.Group(self.apple)
 		
+		self.gameState = 0
+		self.score = 0
+		
 	def isEnd(self):
 		if (self.asteroid1.Xpos == self.penguin.xPos and self.asteroid1.state == self.penguin.state) or (self.asteroid2.Xpos == self.penguin.xPos and self.asteroid2.state == self.penguin.state):
-			self.running = False
+			self.gameState = 2
+	
+	def text_objects(self,text, font):
+		textSurface = font.render(text, True, (0,0,0))
+		return textSurface, textSurface.get_rect()
+	
+	def message(self, text):
+		LargeText = pygame.font.Font('freesansbold.ttf',115)
+		TextSurf, TextRect = self.text_objects(text, LargeText)
+		TextRect.center =(320,200)
+		self.screen.blit(TextSurf,TextRect)
+		
+	def display_score(self, text):
+		normalText = pygame.font.Font('freesansbold.ttf', 20)
+		TextSurf, TextRect = self.text_objects(text, normalText)
+		TextRect.center =(400, 30)
+		self.screen.blit(TextSurf, TextRect)
 		
 	def update(self):
 		
-		if self.penguin.state == 0:
-			self.penguin.yPos = 50
-			
-		elif self.penguin.state == 1:
-			self.penguin.yPos = 150
-			
-		elif self.penguin.state ==2:
-			self.penguin.yPos = 280
-			
-		self.penguin.rect.move(self.penguin.xPos, self.penguin.yPos)
-		
+		self.penguin.update()
 		
 		if self.coloumnXpos <= 100:
 			if self.isEnd():
-				self.running = False
+				self.gameState = 2
 		
 		if self.coloumnXpos <= 0:
 			self.coloumnXpos = 600
-			
+			self.score = self.score + 1
 			random.shuffle(self.coloumn)
 		else:
 			self.coloumnXpos = self.coloumnXpos - .5
@@ -111,9 +132,14 @@ class spaceFruit(object):
 	def render(self):
 		
 		self.screen.fill((255,255,255))
-		self.screen.blit(self.penguin.image, (self.penguin.xPos,self.penguin.yPos))
-		for obj in self.coloumn:
-			self.screen.blit(obj.image, (obj.Xpos,obj.Ypos))
+		if self.gameState == 2:
+			self.message("Score: " + str(self.score))
+		else:
+			self.display_score("Score: " + str(self.score))
+			self.screen.blit(self.penguin.image, (self.penguin.xPos,self.penguin.yPos))
+			self.screen
+			for obj in self.coloumn:
+				self.screen.blit(obj.image, (obj.Xpos,obj.Ypos))
 		pygame.display.flip()
 		
 		
@@ -127,13 +153,24 @@ class spaceFruit(object):
 				if event.type == pygame.KEYDOWN:
 					if event.key == pygame.K_ESCAPE:
 						running = False
-					if event.key == pygame.K_DOWN:
-						self.penguin.state = (self.penguin.state + 1) %3 
-					if event.key == pygame.K_UP:
-						self.penguin.state = (self.penguin.state +2) %3
-						
-			self.update()
-			self.render()
+							
+					if self.gameState == 0:
+						if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
+							self.gameState = 1
+	
+					if self.gameState == 1:
+						if event.key == pygame.K_DOWN:
+							self.penguin.state = (self.penguin.state + 1) %3 
+						if event.key == pygame.K_UP:
+							self.penguin.state = (self.penguin.state +2) %3
+							
+					if self.gameState == 2:
+						self.gameState = 1
+						self.score = -1
+							
+			if self.gameState != 2:	
+				self.update()
+				self.render()
 	
 if __name__ == "__main__":
 	game = spaceFruit()
